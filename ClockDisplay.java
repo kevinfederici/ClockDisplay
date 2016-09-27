1c1c6246927ae75da9447e365e5f1d5f416c71f8
kevinfederici <kfederici64@gmail.com>
2016-09-26 20:35:10 -0400

    changed 24 limit to 13

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -24,7 +24,7 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
@@ -36,7 +36,7 @@ public class ClockDisplay
      */
     public ClockDisplay(int hour, int minute)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
